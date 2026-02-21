cccc99e43e9017da7dd46fc56f67615d5ba2b9c0
Wadney Teixeria <131612853+WadneyT@users.noreply.github.com>
2026-02-21 17:51:29 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello word -UEG 21/02/26! WADNEY AQUI MEU CAMARADA</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
